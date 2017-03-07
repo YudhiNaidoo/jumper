20aa629dd1b76f06ccdf9b418d63004c0e5175c8
Yudhi Naidoo <yudhi@dags.co.za>
2017-03-07 16:13:37 +0200

    Refactor: gulp css and sass tasks

--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,6 @@
     });
 
     // Scripts task
-    // Dont include js from profiles page
     gulp.task('scripts', ['clean-scripts'], function(){
         gulp.src(['css_framework/static/src/js/jquery.js',
             'css_framework/static/src/js/flickity.js',
@@ -39,48 +38,13 @@
     // Watch task
     gulp.task('watch', function(){
         gulp.watch('css_framework/static/src/**/*.js', ['scripts']);
-        gulp.watch('css_framework/static/src/**/*.scss', ['sass']);
         gulp.watch('css_framework/static/src/**/*.css', ['css']);
 
     });
 
-    // Sass task
-    gulp.task('sass', function(){
-        gulp.src('css_framework/static/src/sass/project.scss')
-            .pipe(sourcemaps.init())
-            .pipe(sass().on('error', sass.logError))
-            .pipe(autoprefixer({
-                "browserslist": [
-                    "Chrome",
-                    "Safari",
-                    "Firefox",
-                    "iOS",
-                    "Explorer"
-                  ],
-                cascade: false
-            }))
-            .pipe(concat('project.css'))
-            .pipe(sourcemaps.write())
-            .pipe(gulp.dest('css_framework/static/src/css'));
-    });
 
-
-    // Css task
-    // Dont include css from profiles page
-    gulp.task('css', ['clean-css'], function(){
-        gulp.src('css_framework/static/src/css/*.css')
-            .pipe(sourcemaps.init())
-            .pipe(cssnano())
-            .pipe(concat('final.css'))
-            .pipe(rename({suffix:'.min'}))
-            .pipe(sourcemaps.write())
-            .pipe(gulp.dest('css_framework/static/build/css'));
-    });
-
-
-
-    // Merge Task
-    gulp.task('test', function() {
+    // CSS Task
+    gulp.task('css', function() {
         var streamOne = gulp.src(['css_framework/static/src/css/*.css'])
         .pipe(sourcemaps.init())
         .pipe(sourcemaps.write())
@@ -115,9 +79,8 @@
 
         return mergedStream;
     });
-    // End Merge Task
 
 
 
     // Default task
-    gulp.task('default', ['scripts', 'watch', 'sass', 'css']);
+    gulp.task('default', ['scripts', 'watch', 'css']);
